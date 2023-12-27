d95f95f3d68028720fc9f13af0ef349386d6bed9
shadow-wa <shadwrsi9074@gmail.com>
2023-12-27 12:03:41 +0530

    fix #3 Added HTML web page along side CSS and JS as well as set the route in urls.py

--- chatbotWebServer/chatRailways/urls.py
+++ chatbotWebServer/chatRailways/urls.py
@@ -4,4 +4,5 @@ from . import views
 # urlpatterns that define the URls and their function
 urlpatterns = [
     path('', views.renderWebPage),
+    path('chatbot/', views.startChat, name='startChat')
 ]
